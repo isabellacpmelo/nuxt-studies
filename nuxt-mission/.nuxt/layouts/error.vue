8c54aabbed45cd63977bc1a410642720f0fa28c4
Isabella Melo <42364778+isabellacpmelo@users.noreply.github.com>
2022-12-13 01:51:11 -0300

    Nuxt Data Fetching

---
+++ nuxt-mission/.nuxt/layouts/error.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <p class="error-text">404</p>
+    <div>
+      <span>Take me</span>
+      <nuxt-link to="/">Home</nuxt-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.container {
+  height: viewport;
+  background-color: aqua;
+  text-align: center;
+}
+</style>
--- nuxt-mission/pages/_slug.vue
+++ nuxt-mission/pages/_slug.vue
@@ -9,7 +9,13 @@
     <div class="h-7 mx-auto sm:px-6 lg:px-8">
       <img :src="planet.image" alt="" />
       <h1>{{ planet.title }}</h1>
-      <pre>{{ planet }}</pre>
+      <!-- <p v-if="$fetchState.pending">Fetching planets...</p>
+      <p v-else-if="$fetchState.error">Error while fetchin planets...</p>
+      <ul v-else> -->
+      <!-- <li v-for="planet in planets" :key="planet.slug">
+          <nuxt-link :to="planet.slug">{{ planet.slug }}</nuxt-link>
+        </li> -->
+      <!-- </ul> -->
     </div>
   </div>
 </template>
@@ -19,9 +25,22 @@ export default {
   async asyncData({ params }) {
     const planet = await fetch(
       `https://api.nuxtjs.dev/planets/${params.slug}`
-    ).then((res) => res.json())
+    ).then((res) => {
+      if (res.ok) {
+        return res.json()
+      }
+      throw new Error(res.status)
+    })
 
     return { planet }
   },
+  // data() {
+  //   return { planets: [] }
+  // },
+  // async fetch() {
+  //   this.planets = await fetch(`https://api.nuxtjs.dev/planets`).then((res) =>
+  //     res.json()
+  //   )
+  // },
 }
 </script>
