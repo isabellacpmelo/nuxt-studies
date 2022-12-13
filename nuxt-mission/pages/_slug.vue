37e3d2f07a0c1de148d07cb99e0b55d994391233
Isabella Melo <42364778+isabellacpmelo@users.noreply.github.com>
2022-12-13 01:13:01 -0300

    Starting a project and studying about slug pages

---
+++ nuxt-mission/pages/_slug.vue
@@ -0,0 +1,27 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div
+    class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0"
+  >
+    <h1>
+      {{ $route.params.slug }}
+    </h1>
+    <div class="h-7 mx-auto sm:px-6 lg:px-8">
+      <img :src="planet.image" alt="" />
+      <h1>{{ planet.title }}</h1>
+      <pre>{{ planet }}</pre>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  async asyncData({ params }) {
+    const planet = await fetch(
+      `https://api.nuxtjs.dev/planets/${params.slug}`
+    ).then((res) => res.json())
+
+    return { planet }
+  },
+}
+</script>
